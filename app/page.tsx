b7064603e5504d88a50fd8224fef01e9eb0866e3
Sahil Sahu <imsahilsahu11@gmail.com>
2024-02-06 17:59:40 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import { Inter } from "next/font/google";
 import Image from "next/image";
-import { BiHash, BiHomeCircle } from "react-icons/bi";
+import { BiBell, BiBookmarks, BiEnvelope, BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
 import { BsTwitterX } from "react-icons/bs";
 
-
+const inter=Inter({subsets:["latin"]});
 
 interface TwitterSidebarButton{
   title:string,
@@ -16,13 +17,29 @@ const SidebarMenuItems:TwitterSidebarButton[]=[
   {
     title:'Explore',
     icon:<BiHash/>
-  }
+  },
+  {
+    title:'Notification',
+    icon:<BiBell/>
+  },
+  {
+    title:'Messages',
+    icon:<BiEnvelope/>
+  },
+  {
+    title:'Bookmarks',
+    icon:<BiBookmarks/>
+  },
+  {
+    title:'Profile',
+    icon:<BiUser/>
+  },
 ];
 export default function Home() {
   return (
     <>
     <div className="grid grid-cols-12 h-screen w-screen px-40">
-      <div className="col-span-3 border border-red-300  justify-start pt-4">
+      <div className="col-span-3    justify-start pt-4">
         <div className="text-3xl hover:bg-gray-800 h-fit w-fit rounded-full
         p-2 cursor-pointer transition-all">
         <BsTwitterX />
@@ -30,7 +47,7 @@ export default function Home() {
         <div>
           <ul>
             {SidebarMenuItems.map(item=>
-              <li className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-xl px-4 py-2 w-fit cursor-pointer "
+              <li className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-xl px-4  py-2 w-fit cursor-pointer "
                key={item.title}>
                 <span>{item.icon}</span>
                 <span>{item.title}</span>
